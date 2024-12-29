e80ae0bf7ecdd8a40a151d077406208d1703c0be
Purushothaman S <Purushothamanjerry03@gmail.com>
2024-12-29 19:20:30 +0530

    Added Day2/Operators files

---
+++ factorial.java
@@ -0,0 +1,17 @@
+package day2;
+import java.util.*;
+public class factorial {
+
+	public static void main(String[] args) {
+		
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		int fact=1;
+		for(int i=1;i<=a;i++) {
+			fact*=i;
+		}
+		System.out.println(fact);
+
+	}
+
+}
---
+++ largestnumber.java
@@ -0,0 +1,20 @@
+package day2;
+
+import java.util.Scanner;
+
+public class largestnumber {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner scan=new Scanner(System.in);
+		int a=scan.nextInt();
+		int b=scan.nextInt();
+		int c=scan.nextInt();
+		
+		System.out.println(a>b&&a>c?a:b>a&&b>c?b:c);
+		
+	}
+
+}
