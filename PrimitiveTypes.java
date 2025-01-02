242fb14ca10adbdcbaf6ec24a53c17143d863143
purushothaman S <97541749+purushothamanjerry@users.noreply.github.com>
2025-01-02 15:53:09 +0530

    Add files via upload

---
+++ PrimitiveTypes.java
@@ -0,0 +1,31 @@
+package day1;
+
+public class PrimitiveTypes {
+	
+public static void main (String []args) {
+	
+
+	
+	byte g=127;
+	short a=32627;
+	int b=745672876;
+	long c=987654321;
+	float d=782456.7654321f;
+	double e=8.12345678987654;
+	char f='a';
+	boolean ispossible=false;
+	
+	System.out.println("byte:"+" "+g);
+	System.out.println("short"+" "+a);
+	System.out.println("int"+" "+b);
+	System.out.println("long"+" "+c);
+	System.out.println("float"+" "+d);
+	System.out.println("double"+" "+e);
+	System.out.println("char"+" "+f);
+	System.out.println("boolean"+" "+ispossible);
+	System.out.print("Convert a double to an int"+":"+" ");
+	System.out.println((int)e);
+
+}	
+
+}
