91bcb08eb382505c4ed8b0e249c174862782c5b2
purushothaman S <97541749+purushothamanjerry@users.noreply.github.com>
2025-01-02 15:43:06 +0530

    Add files via upload

---
+++ Day5/Imports and Packages/Main.java
@@ -0,0 +1,16 @@
+import shapes.Rectangle;
+
+import shapes.Circle;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Rectangle rec=new Rectangle(12,20);
+		Circle cir=new Circle(5);
+		
+		rec.areaofRectangle();
+		cir.areaofcircle();
+	}
+
+}
