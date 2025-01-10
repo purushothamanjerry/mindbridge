b6bb4a24843427f677169992694a94e721ba6cf9
purushothaman S <97541749+purushothamanjerry@users.noreply.github.com>
2025-01-10 09:43:40 +0530

    Update ConMurrentMap.java

--- Day11/ConMurrentMap.java
+++ Day11/ConMurrentMap.java
@@ -1,45 +1,44 @@
 package Day11;
-import java.util.*;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.ExecutorService;
 
 import java.util.concurrent.ConcurrentHashMap;
-
-
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-public class ConMurrentMap {
+import java.util.concurrent.TimeUnit;
+
+
+public class ConCurrentMap {
 
 	public static void main(String[] args) {
 		
-	
-	ConcurrentMap<String, Integer> votes = new ConcurrentHashMap<>();
-    ExecutorService executor = Executors.newFixedThreadPool(4);
-    executor.execute(() -> {
-        votes.merge("Alice", 1, Integer::sum);
-    });
+		
+		ConcurrentMap<String, Integer> votes = new ConcurrentHashMap<>();
+		ExecutorService exe = Executors.newFixedThreadPool(4);
+		
+		
+	exe.execute(() -> addVote(votes, "Alice"));
+        exe.execute(() -> addVote(votes, "Bob"));
+        exe.execute(() -> addVote(votes, "Alice"));
+        exe.execute(() -> addCandidateAndVote(votes, "Charlie"));
 
-    executor.execute(() -> {
-        votes.merge("Bob", 1, Integer::sum);
-    });
-
-    executor.execute(() -> {
-        votes.merge("Alice", 1, Integer::sum);
-    });
-
-    executor.execute(() -> {
-        votes.merge("Charlie", 1, Integer::sum);
-    });
-
-    executor.shutdown();
-    while (!executor.isTerminated()) {
+        exe.shutdown();
+        try {
+        	  exe.awaitTermination(1, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Final Vote Counts:");
+        votes.forEach((candidate, count) -> System.out.println(candidate + ": " + count));
     }
-    System.out.println("Final Vote Counts:");
-    votes.forEach((candidate, count) -> {
-        System.out.println(candidate + ": " + count);
-    });
-}
-}
-	
 
+    public static void addVote(ConcurrentMap<String, Integer> votes, String candidate) {
+        votes.compute(candidate, (key, currentCount) -> (currentCount == null) ? 1 : currentCount + 1);
+    }
+    public static void addCandidateAndVote(ConcurrentMap<String, Integer> votes, String candidate) {
+        votes.putIfAbsent(candidate, 0);
+        addVote(votes, candidate);      
+    }
+}	
+	
 
 
