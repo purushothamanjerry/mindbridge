29bd69119370074ae4ea056c78b56a031048fe16
purushothaman S <97541749+purushothamanjerry@users.noreply.github.com>
2025-01-10 09:53:48 +0530

    Update GenericBox.java

--- Day11/GenericBox.java
+++ Day11/GenericBox.java
@@ -1,32 +1,31 @@
 package Day11;
 
-public class GenericBox<T> {
+public class GenericsBox <T>{
 
-   private T item;
-
-	    public T getItem() {
-	        return item;
-	    }
-
-	    public void setItem(T item) {
+		private T item;
+		public void setItem(T item) {
 	        this.item = item;
 	    }
+		
+		 public T getItem() {
+		        return item;
+		    }
+		    public void printItem() {
+		        System.out.println(item);
+		    }
 
-	    public void printItem() {
-	        System.out.println(item);
-	    }
+		    public boolean isTypeOf(Object item) {
+		        return item instanceof String;
+		    }
+		
+		public static void main(String[] args) {
+			   GenericsBox<String> stringBox = new GenericsBox<>();
+		        stringBox.setItem("Hello");
+		        System.out.println("Hello world " + stringBox.isTypeOf(stringBox.getItem()));
+		        System.out.println("Hell the new World? " + stringBox.isTypeOf(Integer.class));
+ }
+		}
 
-	    public boolean isTypeOf(Object item) {
-	        return item instanceof String;
-	    }
-
-	    public static void main(String[] args) {
-	        GenericBox<String> stringBox = new GenericBox<>();
-	        stringBox.setItem("Hello");
-	        System.out.println("Is the item of type String? " + stringBox.isTypeOf(stringBox.getItem()));
-	        System.out.println("Is the item of type Integer? " + stringBox.isTypeOf(new Integer(42)));
-	    }
-	}
 	
 
 
