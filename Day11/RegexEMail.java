c75f4a7898a83a4f0a401a5089a4f818bb1fd575
purushothaman S <97541749+purushothamanjerry@users.noreply.github.com>
2025-01-10 09:23:23 +0530

    Update RegexEMail.java

--- Day11/RegexEMail.java
+++ Day11/RegexEMail.java
@@ -3,23 +3,23 @@ package Day11;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class RegexEMail {
-	
-	
+public class RegMail {
+
 	public static void main(String[] args) {
-        String[] name= {"user@example.com","user.name@domain.org","invalid-email@domain","user@domain_net","@missinguser.com"};
-       String exp="^[a-zA-Z0-9][a-zA-Z0-9.-_]*@[a-zA-Z0-9.-]+\\.(com|org|net)$";
-       Pattern pattern = Pattern.compile(exp);
-       for(String s:name){
-           {
-               Matcher matcher = pattern.matcher(s);
-               if(matcher.matches())
-                   System.out.println(s+": "+"Valid");
-               else
-                   System.out.println(s+": "+"Invalid");
-           }
-       }
-    }
+		
+		String []gmail= {"user@example.com","user.name@domain.org","invalid-email@domain","user@domain_net","@missinguser.com"};
+		String f="^[a-zA-Z0-9][a-zA-Z0-9.-_]*@[a-zA-Z0-9.-]+\\.(com|org|net)$";
+		Pattern p=Pattern.compile(f);
+		
+		for(String s:gmail) {
+			Matcher m=p.matcher(s);
+			if(m.matches()) {
+				System.out.println(s+":"+" "+"Valid");
+			}else {
+				System.out.println(s+":"+" "+"InValid");
+			}
+		}
+		
+	}
+
 }
-
-
